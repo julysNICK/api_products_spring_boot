dac9b2e7303e2c0152446dd7e6649a3d5370d9db
julysNICK <julystube@gmail.com>
2023-10-19 10:23:19 -0300

    add primary notation

--- src/main/java/com/api/products/apiproducts/controller/ProductAdmin.java
+++ src/main/java/com/api/products/apiproducts/controller/ProductAdmin.java
@@ -1,8 +1,13 @@
 package com.api.products.apiproducts.controller;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
 /**
  * ProductAdmin
  */
+@Component
+@Primary
 public class ProductAdmin implements ProductsModel {
 
   @Override
