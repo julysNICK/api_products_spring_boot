7792a3a9471bf8df08a55c69731b7475ac4d2ee2
julysNICK <julystube@gmail.com>
2023-10-20 09:22:25 -0300

    add lifecycles

--- src/main/java/com/api/products/apiproducts/controller/ProductAdmin.java
+++ src/main/java/com/api/products/apiproducts/controller/ProductAdmin.java
@@ -4,15 +4,28 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 /**
  * ProductAdmin
  */
 @Component
-@Scope(org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class ProductAdmin implements ProductsModel {
 
   public ProductAdmin() {
       System.out.println("In constructor: " + getClass().getName());
+  }
+
+
+  @PostConstruct
+  public void init() {
+      System.out.println("In init: " + getClass().getName());
+  }
+
+  @PreDestroy
+  public void destroy() {
+      System.out.println("In destroy: " + getClass().getName());
   }
 
   @Override
